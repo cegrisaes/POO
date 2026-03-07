c76b5171c5aa8e8dc533b150c0d790cee061a0b3
Juleo Sanchez <44029802+cegrisaes@users.noreply.github.com>
2026-03-07 12:38:17 +0100

    Eliminar clase Animal

--- POO_19_La_clase_Object/src/Animal.java
+++
@@ -1,9 +0,0 @@
-public class Animal {
-    public void comer(){
-        System.out.println("El animal come.");
-    }
-
-    public void dormir(){
-        System.out.println("El animal duerme.");
-    }
-}
